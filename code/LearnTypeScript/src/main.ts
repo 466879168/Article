50e2cadf2fea9dc04b5c2ab3b3df011f7f27a002
466879168 <466879168@qq.com>
2019-12-16 17:29:41 +0800

    12.16

--- code/LearnTypeScript/src/main.ts
+++ code/LearnTypeScript/src/main.ts
@@ -1,3 +1,20 @@
-let message:string="hello"
-message="myts"
-console.log(message)
+// function searchXiaoJieJie (age:number):string {
+// return "找到了"+age+"岁的小姐姐"
+// }
+// let age=28
+// let result=searchXiaoJieJie(age)
+// console.log(result);
+
+// let jspang:string = '技术胖'
+// let jspanga:String = new String("jspang.com")
+// console.log(jspang)
+// console.log(jspanga)
+
+
+// let jspang:string = '技术胖'
+// let jspanga:String = new String("jspang.com")
+// console.log(jspang.length)
+// console.log(jspanga.length)
+
+let d:Date = new Date()
+console.log(d)
